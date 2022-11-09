2c996339cbc4f25fea64ea7e126b06b750de44fa
zestgeek39 <ajeet.zestgeek@gmail.com>
2022-11-09 21:21:46 +0530

    updated db name

--- alexaccesories/database.php
+++ alexaccesories/database.php
@@ -7,7 +7,7 @@ if(!defined("DB_HOST")){
 	define("DB_HOST","localhost");
 }
 if(!defined("DB_NAME")){
-	define("DB_NAME","registration");
+	define("DB_NAME","alex-accessories");
 }
 if(!defined("DB_PWD")){
 	define("DB_PWD","password");
