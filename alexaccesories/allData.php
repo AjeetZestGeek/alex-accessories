56f384e6d6340cb302ae64d1f0a633ae43e4f98f
zestgeek39 <ajeet.zestgeek@gmail.com>
2022-11-14 00:10:45 +0530

    updateed update and delete script

--- alexaccesories/allData.php
+++ alexaccesories/allData.php
@@ -1,5 +1,12 @@
 <?php
 include 'layout/header.php';
+if(isset($_GET['id'] )&& isset($_GET['req'])){
+    if($_GET['req']=="delete"){
+        $id = $_GET['id'];
+        $stm = $dbConn->prepare("DELETE  FROM users WHERE id=?");
+        $stm->execute([$id]);
+    }
+}
 $stm = $dbConn->prepare("SELECT * FROM users");
 $stm->execute();
 $all = $stm->fetchAll();
@@ -10,11 +17,12 @@ $all = $stm->fetchAll();
             <div class="container">
     		<thead>
         		<tr>
-        			<th> <mark>id</mark></th>
-        			<th> <mark>Username </mark></th>
+        			<th><mark>id</mark></th>
+        			<th><mark>Username </mark></th>
         			<th><mark>Email Address</mark></th>
         			<th><mark>Phone Number</mark></th>
         			<th><mark>Role</mark></th>
+                    <th><mark>Action</mark></th>
         		</tr>
     	   </thead>
     		<tbody>
@@ -28,7 +36,8 @@ $all = $stm->fetchAll();
                    	<th><?=$val['phonenumber']?></th>
                     <th><?=$val['role']?></th>
            		    <td>
-                        <a class="btn btn-info" href="update.php?id=<?=$val['id']; ?>">Edit</a>&nbsp;<a class="btn btn-danger" href="delete.php?id=<?=$val['id']; ?>&req=delete" onclick='return checkdelete()'>Delete</a>
+                        <a class="btn btn-info" href="update.php?id=<?=$val['id']; ?>">Edit</a>&nbsp
+                        <a class="btn btn-danger" href="?id=<?=$val['id']; ?>&req=delete" onclick='return checkdelete()'>Delete</a>
                     </td>
            	   <?php
                 }
@@ -43,7 +52,6 @@ $all = $stm->fetchAll();
     {
         return confirm('Are you sure you want to delete this user');
     }
-
  </script>
 </div>
 <?php 
