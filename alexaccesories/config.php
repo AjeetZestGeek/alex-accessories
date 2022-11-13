b37a51e17d48d8ecc9b55bfd5da4ec5440ce056f
zestgeek39 <ajeet.zestgeek@gmail.com>
2022-11-14 00:13:14 +0530

    deleted extra files

--- alexaccesories/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost","root","password","crud");
-
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-?>
