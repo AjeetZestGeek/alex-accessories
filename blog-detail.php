8327394a38d81928f340062eac8ddaa33e8e2b40
AjeetZestGeek <ajeet.kumar@zestgeek.com>
2022-12-04 13:55:54 +0530

    Added Search functionality

--- blog-detail.php
+++ blog-detail.php
@@ -1,13 +1,19 @@
 <?php 
 include 'layout/header.php';
+$blogId = $_GET['id'];
+$tableName = 'blog_post';
+$sql = "SELECT $tableName.id as blogId,$tableName.title as blogTitle, blog_categary.title as categoryTitle, content, image, $tableName.created_date as blogCratedDate, username FROM $tableName JOIN blog_categary ON $tableName.category_id = blog_categary.id JOIN users ON $tableName.created_by_id = users.id WHERE $tableName.id = ".$blogId;
+$stm = $dbConn->prepare($sql);
+$stm->execute();
+$data = $stm->fetchAll()[0];
 ?>
 <!-- BLOG DETAIL -->
 <section class="project-detail section-padding-half">
   <div class="container">
     <div class="row">
       <div class="col-lg-9 mx-auto col-md-10 col-12 mt-lg-5 text-center" data-aos="fade-up">
-        <h4 class="blog-category text-info">Creative Work</h4>
-        <h1>9 useful things to learn and practice for your digital marketing</h1>
+        <h4 class="blog-category text-info"><?=$data['categoryTitle']?></h4>
+        <h1><?=$data['blogTitle']?></h1>
         <div class="client-info">
           <div class="d-flex justify-content-center align-items-center mt-3">
             <img src="images/project/project-detail/male-avatar.png" class="img-fluid" alt="male avatar">
@@ -19,25 +25,15 @@ include 'layout/header.php';
     </div>
 </section>
 <div class="full-image text-center" data-aos="zoom-in">
-  <img src="images/blog/blog-header-image.jpg" class="img-fluid" alt="blog header">
+  <img src="alexaccesories/uploads/<?=explode('_', $data['image'])[1]?>" class="img-fluid" alt="blog header">
 </div>
 <!-- BLOG DETAIL -->
 <section class="project-detail">
   <div class="container">
     <div class="row">
       <div class="col-lg-9 mx-auto col-md-11 col-12 my-5 pt-3" data-aos="fade-up">
-        <h2 class="mb-3">Etiam quis metus elementum, tempor risus vel, condimentum orci.</h2>
-        <p>Mauris in convallis nunc, non facilisis arcu. Nunc sapien nulla, interdum at diam non, aliquam vestibulum leo. Fusce laoreet malesuada ante, consectetur consequat ante tempor et. Quisque ac risus ligula.</p>
-        <p>Suspendisse bibendum tortor at est placerat auctor. Phasellus tortor est, bibendum eu ex eu, tincidunt efficitur nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-        <ul class="list-detail py-3">
-          <li><span>Quisque at condimentum est. Duis sollicitudin urna id elit pulvinar placerat. Ut ac dui in ex vulputate dictum.</span></li>
-          <li><span>Mauris vitae tellus nisi. Morbi rutrum lacus sit amet volutpat viverra.</span></li>
-          <li><span> Integer maximus sem ut ipsum blandit elementum. Nullam sollicitudin accumsan commodo.</span></li>
-        </ul>
-        <p>Sed leo nisl, posuere at molestie ac, suscipit auctor mauris. Etiam quis metus elementum, tempor risus vel, condimentum orci.</p>
-        <h2 class="mt-5 mb-3">Curabitur tempus vel libero lobortis feugiat</h2>
-        <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris in convallis nunc, non facilisis arcu. Nunc sapien nulla, interdum at diam non, aliquam vestibulum leo.</p>
-        <blockquote>Phasellus dapibus ex sed dolor blandit, efficitur iaculis ipsum scelerisque. Cras elementum nibh id felis sagittis, sit amet pellentesque ligula porttitor. Fusce laoreet malesuada ante, consectetur consequat ante tempor et. Quisque ac risus ligula.</blockquote>
+        <h2 class="mb-3"><?=$data['categoryTitle']?></h2>
+        <p><?=$data['content']?></p>
       </div>
     </div>
     <div class="col-lg-8 mx-auto mb-5 pb-5 col-12" data-aos="fade-up">
